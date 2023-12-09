20d59522c848e37b03cae486244bd1af40284195
Ethan Beaird <98125550+spectraldecomp@users.noreply.github.com>
2023-12-08 18:50:32 -0600

    Finished day3

--- solutions/day3/day3.py
+++ solutions/day3/day3.py
@@ -3,148 +3,116 @@ import numpy as np
 lines = open('solutions/day3/input.txt').read().splitlines()
 
 def part1():
-    sum = 0
-    # Convert lines to a 2d numpy array
+    # Convert lines to a 2d numpy array and pad with '.' to avoid checking for out of bounds
     matrix = np.array([list(line) for line in lines])
-    
-    # Get the number of rows and columns
+    matrix = np.pad(matrix, pad_width=1, mode='constant', constant_values='.')
     rows, cols = matrix.shape
+    sum = 0
     
-    # Check the first row
-    print("FIRST")
-    col = 0
-    while col < cols:
-        print(col)
-        isPartNumber = True
-        if not matrix[0][col].isnumeric():
-            col+=1
-            continue
-        elif matrix[0][col].isnumeric():
-            # char is a number, check left, right, down, and diagonals
-            checked_places = [matrix[1][col]]
-            if col!=0:
-                checked_places.append(matrix[0][col-1], matrix[1][col-1])
-            if col!=cols-1:
-                checked_places.append(matrix[0][col+1], matrix[1][col+1])
-            if all(place in ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] for place in checked_places):
-                isPartNumber = False
-        if isPartNumber:
-            # Get full number substring, i.e. check to the left and right and get all digits
-            left_start = -1
-            for j in range(col-1, -1, -1):
-                if matrix[0][j].isnumeric() and j == 0:
-                    left_start = 0
-                    break
-                if matrix[0][j].isnumeric():
-                    continue
-                else: 
-                    left_start = j+1
-                    break
-            right_end = -1
-            for j in range(col+1, cols):
-                if matrix[0][j].isnumeric() and j == cols-1:
-                    right_end = cols-1
-                if matrix[0][j].isnumeric():
-                    continue
-                else:
-                    right_end = j
-                    break
-            number = int(''.join(matrix[0, left_start:right_end].tolist()))
-            print(number)
-            sum += number
-            
-            # Continue checking after the place of the last digit of the number
-            col = right_end
-        col += 1
-        
-    print("LAST")
-    # Check the last row
-    col = 0
-    while col < cols:
-        isPartNumber = True
-        if not matrix[rows-1][col].isnumeric():
-            col+=1
-            continue
-        elif matrix[rows-1][col].isnumeric():
-            # char is a number, check left, right, up, and diagonals
-            checked_places = [matrix[rows-1][col-1], matrix[rows-1][col+1], matrix[rows-2][col], matrix[rows-2][col+1], matrix[rows-2][col-1]]
-            if all(place in ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] for place in checked_places):
-                isPartNumber = False
-        if isPartNumber:
-            # Get full number substring, i.e. check to the left and right and get all digits
-            left_start = -1
-            for j in range(col-1, -1, -1):
-                if matrix[rows-1][j].isnumeric() and j == 0:
-                    left_start = 0
-                    break
-                if matrix[rows-1][j].isnumeric():
-                    continue
-                else: 
-                    left_start = j+1
-                    break
-            right_end = -1
-            for j in range(col+1, cols):
-                if matrix[rows-1][j].isnumeric() and j == cols-1:
-                    right_end = cols-1
-                if matrix[rows-1][j].isnumeric():
-                    continue
-                else:
-                    right_end = j
-                    break
-            number = int(''.join(matrix[rows-1, left_start:right_end].tolist()))
-            print(number)
-            sum += number
-            
-            # Continue checking after the place of the last digit of the number
-            col = right_end
-        col += 1
-            
-    # Check the rest of the rows
-    print("REST")
-    for row in range(1, rows-1):
-        col = 0
+    # Check each place in the matrix
+    row = 1
+    while row < rows:
+        col = 1
         while col < cols:
-            isPartNumber = True
-            if not matrix[row][col].isnumeric():
-                col+=1
-                continue
-            elif matrix[row][col].isnumeric():
-                # char is a number, check left, right, up, down, and diagonals
-                checked_places = [matrix[row][col-1], matrix[row][col+1], matrix[row-1][col], matrix[row+1][col], matrix[row-1][col+1], matrix[row-1][col-1], matrix[row+1][col+1], matrix[row+1][col-1]]
-                if all(place in ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] for place in checked_places):
-                    isPartNumber = False
-            if isPartNumber:
-                # Get full number substring, i.e. check to the left and right and get all digits
-                left_start = -1
-                for j in range(col-1, -1, -1):
-                    if matrix[row][j].isnumeric() and j == 0:
-                        left_start = 0
-                        break
-                    if matrix[row][j].isnumeric():
-                        continue
-                    else: 
-                        left_start = j+1
-                        break
-                right_end = -1
-                for j in range(col+1, cols):
-                    if matrix[row][j].isnumeric() and j == cols-1:
-                        right_end = cols-1
-                    if matrix[row][j].isnumeric():
-                        continue
-                    else:
-                        right_end = j
-                        break
-                number = int(''.join(matrix[row, left_start:right_end].tolist()))
-                print(number)
-                sum += number
+            if matrix[row][col].isnumeric():
+                # char is a number, check surrounding places
+                checked_places = [matrix[row-1][col-1], matrix[row-1][col], matrix[row-1][col+1], matrix[row][col-1], matrix[row][col+1], matrix[row+1][col-1], matrix[row+1][col], matrix[row+1][col+1]]
+                if not all(place in ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] for place in checked_places):
+                    # Not all surrounding places are numbers or '.', so this is a part number
+                    # Get full number substring, i.e. check to the left and right and get all digits
+                    left_start = -1
+                    for j in range(col-1, -1, -1):
+                        if matrix[row][j].isnumeric() and j == 1:
+                            left_start = 1
+                            break
+                        if matrix[row][j].isnumeric():
+                            continue
+                        else: 
+                            left_start = j+1
+                            break
+                    right_end = -1
+                    for j in range(col+1, cols):
+                        if matrix[row][j].isnumeric() and j == cols-2:
+                            right_end = cols-2
+                        if matrix[row][j].isnumeric():
+                            continue
+                        else:
+                            right_end = j
+                            break
+                    number = int(''.join(matrix[row, left_start:right_end].tolist()))
+                    sum += number
                 
-                # Continue checking after the place of the last digit of the number
-                col = right_end
+                    # Continue checking after the place of the last digit of the number
+                    col = right_end
             col += 1
-    print(sum)  
-        
-                
-                    
-            
+        row += 1
+    print(sum)
     
+def part2():
+    # Convert lines to a 2d numpy array and pad with '.' to avoid checking for out of bounds
+    matrix = np.array([list(line) for line in lines])
+    matrix = np.pad(matrix, pad_width=1, mode='constant', constant_values='.')
+    rows, cols = matrix.shape
+    sum = 0
+    gears = {}
+    # Check each place in the matrix
+    row = 1
+    while row < rows:
+        col = 1
+        while col < cols:
+            if matrix[row][col].isnumeric():
+                # char is a number, check surrounding places for a gear
+                new_col = -1
+                for i in range(row-1, row+2):
+                    for j in range(col-1, col+2):
+                        place = matrix[i][j]
+                        if place == '*':
+                            # record the coords of the gear
+                            coords = (i, j)
+                        
+                            # Get full number substring, i.e. check to the left and right and get all digits
+                            left_start = -1
+                            for j in range(col-1, -1, -1):
+                                if matrix[row][j].isnumeric() and j == 1:
+                                    left_start = 1
+                                    break
+                                if matrix[row][j].isnumeric():
+                                    continue
+                                else: 
+                                    left_start = j+1
+                                    break
+                            right_end = -1
+                            for j in range(col+1, cols):
+                                if matrix[row][j].isnumeric() and j == cols-2:
+                                    right_end = cols-2
+                                if matrix[row][j].isnumeric():
+                                    continue
+                                else:
+                                    right_end = j
+                                    break
+                                
+                            number = int(''.join(matrix[row, left_start:right_end].tolist()))
+                            
+                            # Add this number to gears
+                            if coords in gears:
+                                gears[coords].append(number)
+                            else:
+                                gears[coords] = [number]
+                        
+                            # Continue checking after the place of the last digit of the number
+                            new_col = right_end
+                if new_col != -1:
+                    col = new_col
+            col += 1
+        row += 1
+        
+    # Multiply each entry of each gear together
+    for gear in gears:
+        if len(gears[gear]) == 2:
+            gears[gear] = np.prod(gears[gear])
+            sum+=gears[gear]
+    print(sum)
+
 part1()
+part2()
+
