29fcf80bb4f1400d71750fb3dfc4421100c4e16f
Ethan Beaird <98125550+spectraldecomp@users.noreply.github.com>
2023-12-20 21:42:18 -0600

    Finished day 16

---
+++ solutions/day16/day16.py
@@ -0,0 +1,75 @@
+import numpy as np
+import sys
+lines = open('solutions/day16/input.txt').read().splitlines()
+arr = np.array([list(line) for line in lines])
+sys.setrecursionlimit(1000000)
+
+
+
+def run_laser(row, col, direction):
+    visited = {(row, col): [] for row in range(arr.shape[0]) for col in range(arr.shape[1])}
+    energized = set()
+    laser(row, col, direction, visited, energized)
+    return len(energized)
+
+def laser(row, col, direction, visited, energized):
+    if direction in visited[(row, col)]:
+        return
+    visited[(row, col)].append(direction)
+    energized.add((row, col))
+    if arr[row][col] == '/':
+        if 'up' in direction:
+            direction = ['right']
+        elif 'right' in direction:
+            direction = ['up']
+        elif 'down' in direction:
+            direction = ['left']
+        elif 'left' in direction:
+            direction = ['down']
+    elif arr[row][col] == '\\':
+        if 'up' in direction:
+            direction = ['left']
+        elif 'right' in direction:
+            direction = ['down']
+        elif 'down' in direction:
+            direction = ['right']
+        elif 'left' in direction:
+            direction = ['up']
+    elif arr[row][col] == '|':
+        if 'left' in direction or 'right' in direction:
+            direction = ['up', 'down']
+    elif arr[row][col] == '-':
+        if 'up' in direction or 'down' in direction:
+            direction = ['left', 'right']
+    for d in direction:
+        if d == 'up' and row-1 >= 0:
+            laser(row-1, col, ['up'], visited, energized)
+        elif d == 'down' and row+1 < arr.shape[0]:
+            laser(row+1, col, ['down'], visited, energized)
+        elif d == 'left' and col-1 >= 0:
+            laser(row, col-1, ['left'], visited, energized)
+        elif d == 'right' and col+1 < arr.shape[1]:
+            laser(row, col+1, ['right'], visited, energized)
+        else:
+            continue
+    return
+
+max_num = 0
+for i in range(arr.shape[0]):
+    num = run_laser(i, 0, ['right'])
+    max_num = max(max_num, num)
+for i in range(arr.shape[0]):
+    num = run_laser(i, arr.shape[1]-1, ['left'])
+    max_num = max(max_num, num)
+for i in range(arr.shape[1]):
+    num = run_laser(0, i, ['down'])
+    max_num = max(max_num, num)
+for i in range(arr.shape[1]):
+    num = run_laser(arr.shape[0]-1, i, ['up'])
+    max_num = max(max_num, num)
+    
+print(max_num)
+
+
+
+            
